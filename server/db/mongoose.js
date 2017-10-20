64ba49e21aef77700501bbb96a4b06cebd8eeb66
montanoa64 <andmontano89@gmail.com>
2017-10-20 18:25:29 -0500

    change the mongoose.connet

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds013172.mlab.com:13172/todo_app' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI|| 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
     mongoose
